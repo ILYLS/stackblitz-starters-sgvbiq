1e89fac798aeec638fd6a3a07874116511c6caa7
ILYLS <36475577+ILYLS@users.noreply.github.com>
2023-06-09 16:09:04 +0200

    2 files modified, 2 added

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,27 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-  `,
+  animations: [
+    trigger('keyframes',[
+      transition(':enter', [       
+        animate('3s', keyframes([ 
+          style({transform: 'translateX(0%)'}), 
+          style({transform: 'translateX(500%)'}), 
+          style({transform: 'translate(500%, 100%)'}), 
+          style({transform: 'translateY(100%)'}), 
+          style({transform: 'translateX(0%)'}) 
+        ])),
+      ])      
+    ])
+  ],
+  templateUrl: 'test.html',
+  styleUrls: ['test.less']
 })
 export class App {
   name = 'zaeeazeaze';
